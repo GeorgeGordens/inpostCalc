bcf55ebf8d9149adcfd134aac77d2a77922d86f6
Alexander Tsetsersky <sanchezlife@tut.by>
2023-01-30 13:17:35 +0300

    add favicon, image, js file and new routes to static files

--- run_app.py
+++ run_app.py
@@ -1,19 +1,40 @@
+import os
+import sys
 import bottle
 
 from beaker.middleware import SessionMiddleware
-from bottle import route, run, template
+from bottle import route, run, static_file, template
 
 from src import config
+
+# root directory of the app, needs for correct routing of static files (e.g. images)
+dir_name = os.path.dirname(sys.argv[0])
 
 
 @route('/')
 @route('/index')
 def page_main():
     content = {
-        'current_user': 'nickname',
+        'current_user': 'username',
         'app_version': '0.0.1'
     }
     return template('index', content)
+
+
+@route('/img/<filename:re:.*\.png>')
+@route('/img/<filename:re:.*\.jpg>')
+def send_img(filename):
+    return static_file(filename, root=dir_name + '/static/img')
+
+
+@route('/js/<filename:re:.*\.js>')
+def send_js(filename):
+    return static_file(filename, root=dir_name + '/static/js')
+
+
+@route('/favicon.ico')
+def send_favicon():
+    return static_file('favicon.ico', root=dir_name + '/static')
 
 
 # Session settings
